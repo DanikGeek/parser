465a2a39e561dbb812ea69c9075cc210ea40f508
DanikGeek <53581368+DanikGeek@users.noreply.github.com>
2023-01-25 16:18:26 +0600

    Add files via upload

---
+++ lesson38-2.php
@@ -0,0 +1,12 @@
+<?php 
+$subject = file_get_contents('https://restoran.kz/restaurant');
+$pattern = '/<a class="link-inherit-color" href="(.+?)">(.+?)<\/a>/u';
+$matches = [];
+preg_match_all($pattern, $subject, $matches);
+print_r($matches);
+
+
+
+
+
+
