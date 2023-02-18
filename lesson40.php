18d503089903a97c3d11cba7a4a34d714c1b3036
DanikGeek <53581368+DanikGeek@users.noreply.github.com>
2023-02-18 11:49:11 +0600

    Update lesson40.php

--- lesson40.php
+++ lesson40.php
@@ -64,6 +64,32 @@
   $rests = array_merge($rests, getRestFromPage($i));
 
   }
-  print_r($rests);
+  //print_r($rests);
+
+
+  $host = '127.0.0.1';
+  $db   = 'cg_rests';
+  $user = 'root';
+  $pass = 'Atbasar123';
+  $charset = 'utf8';
+
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  $opt = [
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES   => false,
+  ];
+  $pdo = new PDO($dsn, $user, $pass, $opt);
+  $stmt = $pdo->prepare("
+    INSERT INTRO
+        `rests` (
+            `name`,
+            `link`,
+            `cuisine`,
+            `price`,
+            `options`
+        )
+
+  ");
 //Урок 34. Базы данных.
 //Часть 5. Программируем добавление в базу данных.
